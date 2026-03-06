5305fb4a88e88b188fb39f5f0f806e50397b1280
KKumarPro <ak23071977@gmail.com>
2026-03-06 21:54:57 +0530

    Student Dashboard Update

--- auth.js
+++ auth.js
@@ -8,6 +8,8 @@ function selectRole(button) {
   button.classList.add("active");
 }
 
+/* SHOW SIGNUP */
+
 function showSignup() {
   document.getElementById("signin-form").style.display = "none";
   document.getElementById("signup-form").style.display = "block";
@@ -16,10 +18,30 @@ function showSignup() {
     "Create your Ami-Bridge account";
 }
 
+/* SHOW SIGNIN */
+
 function showSignin() {
   document.getElementById("signup-form").style.display = "none";
   document.getElementById("signin-form").style.display = "block";
 
   document.getElementById("form-title").innerText =
     "Choose your role and sign in";
+}
+
+/* REDIRECT AFTER LOGIN */
+
+function goToDashboard() {
+  const activeRole = document.querySelector(".role.active").innerText;
+
+  if (activeRole === "Student") {
+    window.location.href = "student-dashboard.html";
+  }
+
+  if (activeRole === "Mentor") {
+    window.location.href = "mentor-dashboard.html";
+  }
+
+  if (activeRole === "Admin") {
+    window.location.href = "admin-dashboard.html";
+  }
 }
